876136cbc149a4e1d1f511fb316712f2ff0f8fe4
plappypanda <marlonfussell@hotmail.com>
2017-06-17 20:56:06 -0700

    Added train and tracks, added new starting platform, stack attack ship and seperate random objects

---
+++ FlippyTurtle/Assets/Scripts/PlayButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Time.timeScale = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Time.timeScale = 1;
+            Destroy(this.gameObject);
+        }
+    }
+
+}
