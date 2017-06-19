7d12ac2098a4e4e93f81c998438172154b77ff3f
plappypanda <marlonfussell@hotmail.com>
2017-06-19 16:03:40 -0700

    added background and store scene and game text

---
+++ FlippyTurtle/Assets/Scripts/StoreScripts/ShopScroll.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShopScroll : MonoBehaviour {
+
+    private float movementStop = 0;
+    private float movementTime = .1f;
+    private bool isMovingLeft = false;
+    private bool isMovingRight = false;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.LeftArrow) && isMovingRight == false)
+        {
+            movementStop = Time.time + movementTime;
+            isMovingLeft = true;
+        }
+
+        if (isMovingLeft == true)
+        {
+            if (movementStop > Time.time)
+            {
+                transform.Translate(Vector2.left * 200 * Time.deltaTime);
+            }
+            else
+            {
+                isMovingLeft = false;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow) && isMovingLeft == false)
+        {
+            movementStop = Time.time + movementTime;
+            isMovingRight = true;
+        }
+
+        if (isMovingRight == true)
+        {
+            if(movementStop > Time.time)
+            {
+                transform.Translate(Vector2.right * 200 * Time.deltaTime);
+            }
+            else
+            {
+                isMovingRight = false;
+            }
+
+        }
+
+
+    }
+}
